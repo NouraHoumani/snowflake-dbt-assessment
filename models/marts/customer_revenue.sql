1ed95b0072171cdc28f34acaeff858b3ada0751f
nourahoumani@outlook.com <nourahoumani@outlook.com>
2025-10-01 23:47:03 +0000

    feat: add staging + marts models and tests

---
+++ models/marts/customer_revenue.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='table') }}
+
+with orders as (
+  select o_orderkey, o_custkey
+  from {{ source('tpch','orders') }}
+),
+customer as (
+  select c_custkey, c_name
+  from {{ source('tpch','customer') }}
+),
+lineitem as (
+  select l_orderkey, l_extendedprice, l_discount
+  from {{ source('tpch','lineitem') }}
+),
+revenue as (
+  select
+    o.o_custkey as customer_key,
+    sum(l.l_extendedprice * (1 - l.l_discount)) as total_revenue
+  from orders o
+  join lineitem l on l.l_orderkey = o.o_orderkey
+  group by 1
+)
+select
+  c.c_custkey as customer_key,
+  c.c_name    as customer_name,
+  r.total_revenue
+from revenue r
+join customer c on c.c_custkey = r.customer_key;
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='view') }}
+
+with orders as (
+  select
+      o_orderkey, o_custkey, o_orderstatus, o_totalprice, o_orderdate,
+      o_orderpriority, o_clerk, o_shippriority, o_comment
+  from {{ source('tpch','orders') }}
+),
+customer as (
+  select c_custkey, c_name
+  from {{ source('tpch','customer') }}
+)
+select
+  o.o_orderkey as order_key,
+  o.o_custkey as customer_key,
+  c.c_name     as customer_name,
+  o.o_orderstatus,
+  o.o_totalprice,
+  o.o_orderdate,
+  extract(year from o.o_orderdate) as order_year,
+  o.o_orderpriority,
+  o.o_clerk,
+  o.o_shippriority,
+  o.o_comment
+from orders o
+left join customer c on o.o_custkey = c.c_custkey;
